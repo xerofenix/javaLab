6ea7480392d817c3efe776c419cd19eda4c3231e
Fenix <xerorianofenix@gmail.com>
2024-02-01 15:19:51 +0530

    name changed sum-diff-prod... to w2q1........

---
+++ w2q1_sum_diff_prod_min_max.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fenix
+ */
+public class w2q1_sum_diff_prod_min_max {
+
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        Scanner sc = new Scanner(System.in);
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+
+        System.out.println("Sum of two numbers: " + (num1 + num2));
+        System.out.println("Difference of two numbers: " + (num1 - num2));
+        System.out.println("Product of two numbers: " + (num1 * num2));
+        System.out.println("Quotient of two numbers: " + (num1 / num2));
+
+        if (num1 > num2) {
+            System.out.println(num1 + "is maximun");
+            System.out.println(num2 + "is minimum");
+        } else {
+            System.out.println(num2 + "is maximum");
+            System.out.println(num1 + "is minimum");
+        }
+
+    }
+}
