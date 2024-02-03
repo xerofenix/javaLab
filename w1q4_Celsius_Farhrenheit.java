0d0ae4eb969717b59eb32ed408ad0570f47cf43d
fenix <xerorianofenix@gmail.com>
2024-02-03 23:20:46 +0530

    modified it

--- w1q4_Celsius_Farhrenheit.java
+++ w1q4_Celsius_Farhrenheit.java
@@ -13,12 +13,13 @@ public class w1q4_Celsius_Farhrenheit {
 
     public static void main(String[] args) {
 
-        System.out.print("Enter temperature in fahrenheit\n");
+        System.out.print("Enter temperature in fahrenheit: ");
         Scanner sc = new Scanner(System.in);
         double fah = sc.nextDouble();
-        double cel = ((fah-32)/9)*5;
+        double cel = ((fah - 32) / 9) * 5;
+        System.out.println("The temperature in Celcius is");
 
-        System.out.println(cel);
-
+        System.out.println(cel + "\u00B0");
+        sc.close();
     }
 }
