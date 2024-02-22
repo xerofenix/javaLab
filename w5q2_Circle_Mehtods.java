8badefee999da9abb692f3cb2623d69170c095b4
xerofenix <xerorianofenix+github@gmail.com>
2024-02-22 14:33:43 +0530

    done, and corrected, now working

---
+++ w5q2_Circle_Mehtods.java
@@ -0,0 +1,20 @@
+public class w5q2_Circle_Mehtods {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * Circle
+ */
+class Circle {
+    double rad;
+
+    void circle(double r) {
+        double area = (22.0 / 7.0) * r * r;
+    }
+
+    void circle(double r) {
+        double peri = 2 * (22.0 / 7.0) * r;
+    }
+}
