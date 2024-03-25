004edad1946f892f94de0fbe07cce8cf9dfb3b0a
xerofenix <xerorianofenix+gitlab@gmail.com>
2024-03-25 12:52:52 +0530

    corrected mistake in loop

--- w3q4_even_sum.java
+++ w3q4_even_sum.java
@@ -7,7 +7,7 @@ public class w3q4_even_sum {
         int n = sc.nextInt();
 
         int sum = 0;
-        for (int i = 2; i <= n * 2; i += 2) {
+        for (int i = 2; i <= n ; i += 2) {
             sum += i;
         }
         System.out.println("The sum all even number form 2 to " + n + " is " + sum);
