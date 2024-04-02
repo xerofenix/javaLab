b9eef4f299dd61371d33e4271f2a76a0a76117c6
xerofenix <xerorianofenix+gitlab@gmail.com>
2024-04-02 11:08:28 +0530

    added some things, need to complete

--- w8q2_Employee.java
+++ w8q2_Employee.java
@@ -1,5 +1,24 @@
-public class w8q2_Employee {
+import java.util.Scanner;
 
+public class w8q2_Employee {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the ID of the employee: ");
+        int id = sc.nextInt();
+        System.out.print("Enter the name of the employee: ");
+        sc.nextLine();
+        String nm = sc.nextLine();
+        System.out.print("Enter the department of thr Employee: ");
+        String dpt = sc.nextLine();
+        System.out.print("Enter the salary of the Employee: ");
+        double slr = sc.nextDouble();
+
+        Employee emp= new Employee(id, nm, dpt, slr);
+
+        emp.display();
+
+
+    }
 }
 
 class Employee {
@@ -18,6 +37,17 @@ class Employee {
     Employee() {
 
     }
+
+    // double accepts(double sal){
+
+    // }
+
+    void display() {
+        System.out.println("The name of the employee is " + this.name);
+        System.out.println("The id of the employee is " + this.id);
+        System.out.println("The department of the employee is " + this.department);
+        System.out.println("The salary of the employee is " + this.salary);
+    }
 }
 
 class Manager extends Employee {
@@ -27,4 +57,10 @@ class Manager extends Employee {
         super(id, name, department, salary);
         this.bonus = bonus;
     }
+
+void display ()
+{
+    System.out.println("The bonus of the Manager is "+this.bonus);
+}
+
 }
