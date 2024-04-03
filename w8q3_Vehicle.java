fae52f280c0383958d75baed9009564c9f2f9eb7
xerofenix <xerorianofenix+gitlab@gmail.com>
2024-04-03 17:46:25 +0530

    work in progress

--- w8q3_Vehicle.java
+++ w8q3_Vehicle.java
@@ -4,14 +4,47 @@ public class w8q3_Vehicle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        String in;
+        String inp;
+        int flag = 0, check = 0;
         while (true) {
             System.out.println("Enter 1 for Light Motor Vehicle, 2 for Heavy Motor Vehicle, Q/q for exit");
-            in = sc.next();
-            int check = Integer.parseInt(in);
-            if (in.equalsIgnoreCase("Q")) {
+            inp = sc.next();
+            sc.nextLine();
+            if (inp.equalsIgnoreCase("Q")) {
                 break;
+            }
+            try {
+                check = Integer.parseInt(inp);
+
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Invalid input");
+            }
+
+            switch (check) {
+                case 1:
+                    System.out.print("Enter the company name: ");
+                    String cmp_name_lmv = sc.nextLine();
+                    System.out.print("Enter the price of the vehicle: ");
+                    double price_lmv = sc.nextDouble();
+                    System.out.print("Enter the mileage of the vehicle: ");
+                    double milag = sc.nextDouble();
+                    LightMotorVehicle lmv = new LightMotorVehicle(cmp_name_lmv, price_lmv, milag);
+                    lmv.toShow();
+                    break;
+                case 2:
+                    System.out.print("Enter the company name: ");
+                    String cmp_name_hmv = sc.nextLine();
+                    System.out.print("Enter the price of the vehicle: ");
+                    double price_hmv = sc.nextDouble();
+                    System.out.print("Enter the mileage of the vehicle: ");
+                    double capc = sc.nextDouble();
+                    LightMotorVehicle hmv = new LightMotorVehicle(cmp_name_hmv, price_hmv, capc);
+                    hmv.toShow();
+                    break;
+
+                default:
+                    break;
             }
 
         }
@@ -31,11 +64,17 @@ class Vehicle {
 }
 
 class LightMotorVehicle extends Vehicle {
-    String mileage;
+    double mileage;
 
-    LightMotorVehicle(String company, double price, String mileage) {
+    LightMotorVehicle(String company, double price, double mileage) {
         super(company, price);
         this.mileage = mileage;
+    }
+
+    void toShow() {
+        System.out.print("The company of the vehicle is " + this.company);
+        System.out.print("The price of the vehicle is " + this.price);
+        System.out.print("The mileage of the vehicle is " + this.mileage);
     }
 }
 
@@ -45,5 +84,11 @@ class HeavyMotorVehicle extends Vehicle {
     HeavyMotorVehicle(String company, double price, double capacityInTons) {
         super(company, price);
         this.capacityInTons = capacityInTons;
+    }
+
+    void toShow() {
+        System.out.print("The company of the vehicle is " + this.company);
+        System.out.print("The price of the vehicle is " + this.price);
+        System.out.print("The capacity of the vehicle is " + this.capacityInTons + " tons");
     }
 }
