f0c5f98ca05a25be0176f2a170f42fc0f2700df3
xerofenix <xerorianofenix+gitlab@gmail.com>
2024-04-28 15:31:55 +0530

    making aritmatic packages and doing operation done

---
+++ arithmatics/Arithmatic.java
@@ -0,0 +1,26 @@
+ package arithmatics;
+
+public class Arithmatic {
+
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	public int mul(int x, int y) {
+		return x*y;
+	}
+	public double div(int x, int y) {
+		return (double)x/y;
+	}
+	public int mod(int x, int y) {
+		return x%y;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ arithmatics/w9q5_Arithmatics.java
@@ -0,0 +1,14 @@
+package arithmatics;
+// package weeks;
+// import arithmatics.AW9Q5;
+
+public class w9q5_Arithmatics {
+	public static void main(String[] args) {
+		Arithmatic m =new Arithmatic();
+		System.out.println(m.add(8, 5));
+		System.out.println(m.sub(8, 5));
+		System.out.println(m.mul(8, 5));
+		System.out.println(m.div(8, 5));
+		System.out.println(m.mod(8, 5));
+	}
+}
